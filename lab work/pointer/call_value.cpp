039339fda50e2b9759c3888630b85a3f166adedd
yashvi_vadodariya <yashvivadodariya72@gmail.com>
2025-05-30 02:52:45 -0400

    Add files via upload

---
+++ lab work/pointer/call_value.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void inc(int x);
+
+main(){
+	int a=5;
+	inc(a);
+	
+	cout<<"After A value :"<<a<<endl;
+}
+void inc(int x){
+	x=x+1;
+	
+	cout<<"Befor A value :"<<x<<endl;
+}
